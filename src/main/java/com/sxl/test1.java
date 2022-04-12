591e0579f13df575c9a80fa063e11bcc70258d97
lan <lan@sjzu.com>
2022-04-12 20:38:26 +0800

    first commit

---
+++ src/main/java/com/sxl/test1.java
@@ -0,0 +1,11 @@
+package com.sxl;
+
+/**
+ * @author lan
+ * @date 2022/4/12-20:27
+ */
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("这是java连接git");
+    }
+}
