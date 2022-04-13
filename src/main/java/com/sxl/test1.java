57161b52c2c1080044513bfb5a8371ac5d1c3eb2
lan <lan@sjzu.com>
2022-04-13 09:49:53 +0800

    hot first commit

--- src/main/java/com/sxl/test1.java
+++ src/main/java/com/sxl/test1.java
@@ -8,5 +8,6 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("这是java连接git");
         System.out.println("这是版本2");
+        System.out.println("这是热修");
     }
 }
